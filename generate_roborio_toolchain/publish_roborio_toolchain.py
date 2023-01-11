740c4e6c7a0a351458ebb6cc2bff1a3379f3ec9a
PJ Reiniger <pj.reiniger@gmail.com>
2023-01-11 03:46:38 -0500

    More

---
+++ generate_roborio_toolchain/publish_roborio_toolchain.py
@@ -0,0 +1,22 @@
+
+import os
+from get_toolchain_dependencies import get_toolchain_dependencies
+from bazelrio_gentool.generate_json import generate_json
+
+def main():
+    SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+    registry_location = os.path.join(SCRIPT_DIR, "..", '..', "bazel-central-registry")
+    print(registry_location)
+    print(os.path.exists(registry_location))
+
+    group = get_toolchain_dependencies()
+    
+    module_json_template = os.path.join(SCRIPT_DIR, "module_config.json.jinja2")
+    print(module_json_template)
+
+    os.chdir(SCRIPT_DIR)
+    generate_json(registry_location, group, module_json_template)
+
+
+if __name__ == "__main__":
+    main()
