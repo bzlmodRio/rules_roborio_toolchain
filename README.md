72e3d0159c709c752060a5edcecbbd24468a43a5
PJ Reiniger <pj.reiniger@gmail.com>
2023-01-18 17:04:25 -0500

    More

--- README.md
+++
@@ -1,1 +0,0 @@
-# rules_roborio_toolchain
--- generate_roborio_toolchain/generate_toolchain_deps.py
+++ generate_roborio_toolchain/generate_toolchain_deps.py
@@ -3,9 +3,7 @@ import os
 from generate_roborio_toolchain.get_toolchain_dependencies import get_toolchain_dependencies
 
 from bazelrio_gentool.generate_toolchain import generate_toolchain
-# from bazelrio_gentool.generate_module_project_files import generate_module_project_files, MandetoryDependencySettings
-# from bazelrio_gentool.clean_existing_version import clean_existing_version
-# from bazelrio_gentool.utils import TEMPLATE_BASE_DIR, render_template
+from bazelrio_gentool.clean_existing_version import clean_existing_version
 
 
 
@@ -16,21 +14,9 @@ def main():
 
     group = get_toolchain_dependencies()
     print(group)
-
+    
+    clean_existing_version(REPO_DIR)
     generate_toolchain(REPO_DIR, group)
-
-    # rules_roborio_toolchain_dep = MandetoryDependencySettings.Setting(0, True)
-    # rules_bazelrio_dep = MandetoryDependencySettings.Setting(0, True)
-
-    # mandetory_dependencies = MandetoryDependencySettings(
-    #     bcr_branch = "main",
-    #     rules_roborio_toolchain=rules_roborio_toolchain_dep,
-    #     rules_bazelrio=rules_bazelrio_dep,
-    # )
-
-    # # clean_existing_version(REPO_DIR)
-    # generate_module_project_files(REPO_DIR, group, mandetory_dependencies)
-    # generate_group(output_dir, group)
     
 if __name__ == "__main__":
     main()
