dac7556a44e12ab6cb031c38d76074024d349379
PJ Reiniger <pj.reiniger@gmail.com>
2023-05-05 22:21:12 -0400

    gen

--- generate/generate.py
+++ generate/generate.py
@@ -6,7 +6,7 @@ from bazelrio_gentool.clean_existing_version import clean_existing_version
 
 
 def main():
-    SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+    SCRIPT_DIR = os.environ["BUILD_WORKSPACE_DIRECTORY"]
     REPO_DIR = os.path.join(SCRIPT_DIR, "..")
     output_dir = os.path.join(REPO_DIR, "dependencies")
 
--- generate/publish.py
+++ generate/publish.py
@@ -4,7 +4,7 @@ from bazelrio_gentool.generate_json import generate_json
 
 
 def main():
-    SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+    SCRIPT_DIR = os.environ["BUILD_WORKSPACE_DIRECTORY"]
     registry_location = os.path.join(
         SCRIPT_DIR, "..", "..", "..", "bazel-central-registry"
     )
